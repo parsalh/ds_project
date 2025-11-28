952307f726fe53e1a3dd2f317aa848ae049da153
Stavroula Parsali <it2022088@hua.gr>
2025-11-28 19:23:45 +0200

    Finished repositories

--- src/main/java/gr/hua/dit/project/core/repository/OpenHourRepository.java
+++ src/main/java/gr/hua/dit/project/core/repository/OpenHourRepository.java
@@ -1,4 +1,25 @@
 package gr.hua.dit.project.core.repository;
-//TODO implement
-public interface OpenHourRepository {
+
+import gr.hua.dit.project.core.model.DayOfWeek;
+import gr.hua.dit.project.core.model.OpenHour;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Repository for managing {@link OpenHour} entity.
+ */
+@Repository
+public interface OpenHourRepository extends JpaRepository<OpenHour, Long> {
+
+    List<OpenHour> findAllByRestaurantId(final Long restaurantId);
+
+    Optional<OpenHour> findByRestaurantIdAndDayOfWeek(final Long restaurantId, final DayOfWeek dayOfWeek);
+
+    boolean existsByRestaurantIdAndDayOfWeek(final Long restaurantId, final DayOfWeek dayOfWeek);
+
+    boolean existsByRestaurantId(final Long restaurantId);
+
 }
--- src/main/java/gr/hua/dit/project/core/repository/OrderItemRepository.java
+++ src/main/java/gr/hua/dit/project/core/repository/OrderItemRepository.java
@@ -1,4 +1,23 @@
 package gr.hua.dit.project.core.repository;
-// TODO implement
-public interface OrderItemRepository {
+
+import gr.hua.dit.project.core.model.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Repository for managing {@link OrderItem} entity.
+ */
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem,Integer> {
+
+    List<OrderItem> findAllByOrderId(final Long orderId);
+
+    List<OrderItem> findAllByMenuItemId(final Long menuItemId);
+
+    List<OrderItem> findAllByNameIgnoreCase(final String name);
+
+    boolean existsByMenuItemId(final Long menuItemId);
+
 }
