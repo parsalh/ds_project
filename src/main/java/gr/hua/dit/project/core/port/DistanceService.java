df121cccdbb7cab276c722e8545077d253cd961e
Stavroula Parsali <it2022088@hua.gr>
2025-12-14 18:19:19 +0200

    Implemented distance calculator with ORS

--- src/main/java/gr/hua/dit/project/core/port/DistanceService.java
+++ src/main/java/gr/hua/dit/project/core/port/DistanceService.java
@@ -4,11 +4,16 @@ import java.util.Optional;
 
 public interface DistanceService {
 
+    /**
+     * Calculates distances between two locations.
+     * @return Distance and duration item.
+     */
     Optional<RouteMetrics> getDistanceAndDuration(double startLat,
                                                   double startLon,
                                                   double endLat,
                                                   double endLon);
 
+    // DTO
     record RouteMetrics(double distanceMeters, double durationSeconds) {}
 
 }
