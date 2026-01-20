65a9d106b54c7e003ec7e22db7e92046f206c19e
Stavroula Parsali <it2022088@hua.gr>
2026-01-20 14:03:06 +0200

    ok?

--- src/main/java/gr/hua/dit/project/web/rest/SmsResource.java
+++ src/main/java/gr/hua/dit/project/web/rest/SmsResource.java
@@ -1,6 +1,6 @@
 package gr.hua.dit.project.web.rest;
 
-import gr.hua.dit.project.core.port.SmsService;
+import gr.hua.dit.project.core.port.SmsNotificationPort;
 import gr.hua.dit.project.core.port.impl.dto.SendSmsRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,22 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class SmsResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SmsResource.class);
-    private final SmsService smsService;
+    private final SmsNotificationPort smsNotificationPort;
 
-    public SmsResource(final SmsService smsService) {
-        if (smsService == null) throw new NullPointerException();
-        this.smsService = smsService;
+    public SmsResource(final SmsNotificationPort smsNotificationPort) {
+        if (smsNotificationPort == null) throw new NullPointerException();
+        this.smsNotificationPort = smsNotificationPort;
     }
 
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> sendSms(@RequestBody SendSmsRequest sendSmsRequest) {
         try {
-            this.smsService.send(sendSmsRequest.e164(), sendSmsRequest.content());
-            return ResponseEntity.ok("SENT");
+            boolean sent = this.smsNotificationPort.sendSms(sendSmsRequest.e164(), sendSmsRequest.content());
+            if (sent) {
+                return ResponseEntity.ok("SENT");
+            } else {
+                return ResponseEntity.status(500).body("FAILED");
+            }
         } catch (Exception ex) {
             LOGGER.error("Failed to send SMS {}", sendSmsRequest, ex);
             return ResponseEntity.status(500).body("FAILED");
-
         }
     }
 }
